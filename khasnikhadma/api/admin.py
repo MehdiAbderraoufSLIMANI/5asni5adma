7d177efcba459c6de94e82357263d05fb1d27f73
justmahdii <didoslimani10@gmail.com>
2023-07-08 19:13:02 +0100

    adding tables of Artisan and Client and user

--- khasnikhadma/api/admin.py
+++ khasnikhadma/api/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
  
-from .models import usertest
+from .models import usertest,Artisan,Client
 
 
 admin.site.register(usertest)
+
+
+#Artisan""""""""""""""""""""""""""""""""
+@admin.register(Artisan)
+class ArtisanAdmin(admin.ModelAdmin):
+    readonly_fields = ('rating','compte_type')
+
+
+#Client""""""""""""""""""""""""""""""""
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    readonly_fields = ('compte_type',)
--- khasnikhadma/api/models.py
+++ khasnikhadma/api/models.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.db import models
 
  
@@ -5,3 +6,54 @@ from django.db import models
 class usertest(models.Model):
     name = models.TextField(max_length=50 ,default=" ")
     password = models.TextField(max_length=50 ,default=" ")
+
+class User(models.Model):
+    #id_compte = models.TextField(max_length=50 ,default=" ")
+    id_compte = models.AutoField(primary_key=True, db_column='copmt_id', verbose_name='compte id')
+
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=50) 
+    
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    tel = models.IntegerField()
+
+    isBanned = models.BooleanField()
+    
+    CATEGORY_CHOICES = [
+        ('client', 'Client'),
+        ('worker', 'Worker'),
+    ]
+    
+    #compte_type = models.CharField(max_length=10, choices=CATEGORY_CHOICES, default="client")
+    compte_type = models.CharField(max_length=10 ,editable=False) 
+
+    def save(self, *args, **kwargs):
+        # Set the default compte_type based on the model subclass being created
+        if isinstance(self, Artisan):
+            self.compte_type = "worker"
+        elif isinstance(self, Client):
+            self.compte_type = "client"
+        super().save(*args, **kwargs)
+
+class Artisan(User):
+    numb_card_national = models.IntegerField()
+    wilaya = models.CharField(max_length=50)
+    commune = models.CharField(max_length=50)
+    adresse = models.CharField(max_length=50)
+    rating = models.FloatField(max_length=1, editable=False, default=0)
+    category_of_worker = models.CharField(max_length=10, default=" ")
+    
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.compte_type = "worker"
+
+class Client(User):
+    wilaya = models.CharField(max_length=50)
+    commune = models.CharField(max_length=50)
+    adresse = models.CharField(max_length=50)
+    
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.compte_type = "client"

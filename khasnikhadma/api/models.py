65ce6d99df5828127142c50a4899d6fdfec56606
justmahdii <didoslimani10@gmail.com>
2023-07-08 17:59:49 +0100

    test2 of merging

--- khasnikhadma/api/models.py
+++ khasnikhadma/api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
  
-#testing
+#testing 2
 class usertest(models.Model):
     name = models.TextField(max_length=50 ,default=" ")
     password = models.TextField(max_length=50 ,default=" ")
